fa7c932358f303dbb2025d32d56c29982022d25e
Alexander Nair <alexnair@me.com>
2026-01-22 16:44:01 -0700

    zones showing up

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -3,30 +3,65 @@ import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 
 export default function Map() {
-  const mapContainer = useRef(null);
+    const mapContainer = useRef(null);
 
-  useEffect(() => {
-    const map = new maplibregl.Map({
-      container: mapContainer.current,
-      style: "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json",
-      center: [-106.4850, 31.7619], // El Paso
-      zoom: 11,
-    });
+    useEffect(() => {
+        const map = new maplibregl.Map({
+            container: mapContainer.current,
+            style: "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json",
+            center: [-106.4850, 31.7619], // El Paso
+            zoom: 11,
+        });
 
-    map.addControl(new maplibregl.NavigationControl(), "top-right");
+        map.addControl(new maplibregl.NavigationControl(), "top-right");
+        map.on("load", async () => {
+            // Add zoning source
+            map.addSource("zoning", {
+                type: "geojson",
+                data: "/data/zoning.geojson",
+            });
 
-    return () => map.remove();
-  }, []);
+            // Add zoning layer
+            map.addLayer({
+                id: "zoning-fill",
+                type: "fill",
+                source: "zoning",
+                paint: {
+                    "fill-color": [
+                        "match",
+                        ["get", "ZONELABEL"],
+                        "C1", "#4CAF50",
+                        "C2", "#2E7D32",
+                        "C3", "#1B5E20",
+                        "#CCCCCC" // default
+                    ],
+                    "fill-opacity": 0.5,
+                },
+            });
 
-  return (
-  <div
-    ref={mapContainer}
-    style={{
-      position: "absolute",
-      top: 0,
-      bottom: 0,
-      width: "100%",
-    }}
-  />
-);
+            // Optional outline
+            map.addLayer({
+                id: "zoning-outline",
+                type: "line",
+                source: "zoning",
+                paint: {
+                    "line-color": "#555",
+                    "line-width": 0.5,
+                },
+            });
+        });
+        return () => map.remove();
+    }, []);
+
+    return (
+        <div
+            ref={mapContainer}
+            style={{
+                position: "absolute",
+                top: 0,
+                bottom: 0,
+                width: "100%",
+            }}
+        />
+    );
 }
